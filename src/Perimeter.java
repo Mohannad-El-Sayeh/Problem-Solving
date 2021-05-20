253fa84e0600f86be19a9a18947da047a69ce5d0
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2021-05-20 20:45:46 +0200

    Perimeter of a rectangle

---
+++ src/Perimeter.java
@@ -0,0 +1,7 @@
+// Link for the Problem : https://edabit.com/challenge/cMKLM7ExBd3TFJwPA
+
+public class Perimeter {
+    public static int findPerimeter(int length, int width) {
+        return length*2 + width*2;
+    }
+}
