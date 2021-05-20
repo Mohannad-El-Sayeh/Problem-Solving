94cc7c65d5cacb2c4bce43465286b05f31cdca4f
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2021-05-20 20:35:34 +0200

    Area of triangle

---
+++ src/AreaOfTriangle.java
@@ -0,0 +1,7 @@
+// Link for the problem : https://edabit.com/challenge/B4HjZbBh9yuELmjna
+
+public class AreaOfTriangle {
+    public static int triArea(int base, int height) {
+        return base * height / 2;
+    }
+}
