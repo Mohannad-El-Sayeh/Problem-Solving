fac7867e6ccf15c3faa0195937c00ce9f8f16290
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2021-05-21 07:20:50 +0200

    Basic Calculator

---
+++ src/BasicCalculator.java
@@ -0,0 +1,21 @@
+// Link of the problem : https://edabit.com/challenge/gyfsGx7KrGLscxFrD
+
+public class BasicCalculator {
+    public static int calculator(int num1, char operator, int num2) {
+        switch (operator){
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if(num2 == 0){
+                    return 0;
+                }
+                return num1 / num2;
+            default:
+                return 0;
+        }
+    }
+}
